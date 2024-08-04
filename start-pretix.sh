697468182823c02f3a9cbea9b6132f748787522b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-08-04 13:26:06 +0800

    Update start-pretix.sh

--- start-pretix.sh
+++ start-pretix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 启动Pretix的命令，可以根据需要调整
-docker run --name pretix -p 80:80 \
+docker run --name pretix -p 8000:8000 \
     -v /var/pretix-data:/data \
     -v /etc/pretix:/etc/pretix \
     pretix/standalone:stable
